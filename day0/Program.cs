019fcd9d2e78206d8487d6971cf0b370ac9d787e
Matthew Sharpe <mds@gn.apc.org>
2019-11-29 20:44:29 +0000

    add gitignore, test .net 3 console app

---
+++ day0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
