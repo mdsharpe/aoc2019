0dafd9418fd1b3a3bc34e24a9a55afd342615497
Matthew Sharpe <mds@gn.apc.org>
2019-12-01 07:50:34 +0000

    day 1 part 1

---
+++ day1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                File.ReadAllLines(args[0])
+                    .Select(o => int.Parse(o))
+                    .Select(CalculateFuel)
+                    .Sum()
+            );
+        }
+
+        private static int CalculateFuel(int mass)
+            => (int)Math.Floor((decimal)mass / 3) - 2;
+    }
+}
