eb67d0fea0da8f9f1313498a53f63c6a332703dd
Matthew Sharpe <matthew.sharpe@enable.com>
2019-12-06 12:51:05 +0000

    day4 part2

--- day4/Program.cs
+++ day4/Program.cs
@@ -10,23 +10,57 @@ namespace day4
             int rangeStart = int.Parse(args[0]),
                 rangeEnd = int.Parse(args[1]);
 
-            var result = Enumerable.Range(rangeStart, (rangeEnd - rangeStart) + 1)
-                .Where(o => TwoAdjacentDigitsTheSame(o))
-                .Where(o => !DigitsDecrease(o))
-                .Count();
+            var range = Enumerable.Range(rangeStart, (rangeEnd - rangeStart) + 1);
 
-            Console.WriteLine(result);
+            var part1Possibilities = range
+                .Where(o => HasAdjacentDigits(o))
+                .Where(o => !DigitsDecrease(o));
+
+            var part1Result = part1Possibilities.Count();
+
+            Console.WriteLine($"Part 1 result: {part1Result}");
+
+            var part2Possibilities = range
+                .Where(o => HasAdjacentDigits(o, 2))
+                .Where(o => !DigitsDecrease(o));
+
+            var part2Result = part2Possibilities.Count();
+
+            Console.WriteLine($"Part 2 result: {part2Result}");
         }
 
-        private static bool TwoAdjacentDigitsTheSame(int n)
+        private static bool HasAdjacentDigits(int n, int? requiredSequenceLength = null)
         {
             var s = n.ToString();
+            var sequenceLength = 1;
 
             for (var i = 1; i < s.Length; i++)
             {
-                if (s[i] == s[i - 1])
+                var isSameAsPrevious = s[i] == s[i - 1];
+
+                if (isSameAsPrevious)
                 {
-                    return true;
+                    sequenceLength++;
+                }
+
+                if (sequenceLength > 1)
+                {
+                    if (requiredSequenceLength.HasValue)
+                    {
+                        if ((!isSameAsPrevious || i == s.Length - 1) && sequenceLength == requiredSequenceLength.Value)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+
+                if (!isSameAsPrevious)
+                {
+                    sequenceLength = 1;
                 }
             }
 
